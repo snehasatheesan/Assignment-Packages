d346b596988d4d4afa6c9fbbfebe7362b399921a
snehasatheesan <snehasatheesan13@gmail.com>
2024-10-04 10:33:43 +0530

    Update Student.java

--- com.assignment15/src/university/students/Student.java
+++ com.assignment15/src/university/students/Student.java
@@ -1,4 +1,18 @@
-package university.students;
+package com.assignment15.university.students;
 
 public class Student {
+    private String name;
+    private static int rollNo=100;
+    public Student(String name){
+        this.name=name;
+        rollNo++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static int getRollNo() {
+        return rollNo;
+    }
 }
