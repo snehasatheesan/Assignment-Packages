99d8d0100d16dbb7763c75ea2f8406cc12262372
snehasatheesan <snehasatheesan13@gmail.com>
2024-10-04 10:33:15 +0530

    Update faculty.java

--- com.assignment15/src/university/faculty/faculty.java
+++ com.assignment15/src/university/faculty/faculty.java
@@ -1,4 +1,19 @@
-package university.faculty;
 
-public class faculty {
+package com.assignment15.university.faculty;
+
+public class Faculty {
+    private String name;
+    private static int facultyId=2000;
+    public Faculty(String name){
+        this.name=name;
+        facultyId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static int getFacultyId() {
+        return facultyId;
+    }
 }
